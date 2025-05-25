1230355c202d0a470e5870bda45393cec3da7644
mqcdev <michael.chavezjq@gmail.com>
2025-05-24 19:13:20 -0500

    chore(customer): initialize project

---
+++ src/test/java/com/nttdata/banking/customer/BankingCustomerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nttdata.banking.customer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankingCustomerServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

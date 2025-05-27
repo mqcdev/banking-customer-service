9c6bc6f52db6bf4057f3ceb832bd09a35c93d5a4
mqcdev <michael.chavezjq@gmail.com>
2025-05-26 19:42:07 -0500

    feat(repository): update CustomerRepository

--- src/main/java/com/nttdata/banking/customer/repositories/CustomerRepository.java
+++ src/main/java/com/nttdata/banking/customer/repositories/CustomerRepository.java
@@ -1,6 +1,6 @@
 package com.nttdata.banking.customer.repositories;
 
-import com.nttdata.banking.customer.models.Customer;
+import com.nttdata.banking.customer.models.entity.Customer;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
 import org.springframework.stereotype.Repository;
 import reactor.core.publisher.Flux;
@@ -8,9 +8,27 @@ import reactor.core.publisher.Mono;
 
 @Repository
 public interface CustomerRepository extends ReactiveMongoRepository<Customer, String> {
+
+    Mono<Customer> findByEmail(String email);
+
     Mono<Customer> findByDocumentNumber(String documentNumber);
 
     Mono<Customer> findByRuc(String ruc);
 
-    Flux<Customer> findByActive(Boolean active);
+    Mono<Boolean> existsByEmail(String email);
+
+    Mono<Boolean> existsByDocumentNumber(String documentNumber);
+
+    Mono<Boolean> existsByRuc(String ruc);
+
+    Flux<Customer> findByActiveTrue();
+
+    Flux<Customer> findByActiveTrueAndCustomerTypeContaining(String customerType);
+
+    Mono<Boolean> existsByEmailAndIdNot(String email, String id);
+
+    Mono<Boolean> existsByDocumentNumberAndIdNot(String documentNumber, String id);
+
+    Mono<Boolean> existsByRucAndIdNot(String ruc, String id);
 }
+

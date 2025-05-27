dfc2aa8b4498dea356abd2e6db5854d6a380f171
mqcdev <michael.chavezjq@gmail.com>
2025-05-27 17:05:35 -0500

    feat(config): update OpenAPIConfig

--- src/main/java/com/nttdata/banking/customer/config/OpenAPIConfig.java
+++ src/main/java/com/nttdata/banking/customer/config/OpenAPIConfig.java
@@ -1,4 +1,50 @@
 package com.nttdata.banking.customer.config;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.servers.Server;
+
+import java.util.List;
+
+@Configuration
 public class OpenAPIConfig {
+    @Value("${nttdata.openapi.dev-url}")
+    private String devUrl;
+    @Value("${nttdata.openapi.prod-url}")
+    private String prodUrl;
+
+    @Bean
+    public OpenAPI openAPI() {
+        Server devServer = new Server();
+        devServer.setUrl(devUrl);
+        devServer.setDescription("Development server");
+        Server prodServer = new Server();
+        prodServer.setUrl(prodUrl);
+        prodServer.setDescription("Production server");
+
+        Contact contact = new Contact();
+        contact.setName("Nttdata Banking");
+        contact.setEmail("nttdata@nttdata.com");
+        contact.setUrl("https://nttdata.com");
+
+        License mirLicense = new License().name("MIT").url("https://opensource.org/licenses/MIT");
+
+        Info info = new Info();
+        info.setTitle("Banking API");
+        info.setDescription("API for managing banking customer");
+        info.setVersion("1.0.0");
+        info.setContact(contact);
+        info.setLicense(mirLicense);
+        info.termsOfService("https://nttdata.com/terms-of-service");
+
+        return new OpenAPI()
+                .info(info)
+                .servers(List.of(devServer, prodServer));
+    }
+
 }
